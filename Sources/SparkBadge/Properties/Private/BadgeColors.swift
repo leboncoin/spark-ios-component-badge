d33835f38fc3deb80755fa84488ea36307467f1d
Robin Lemaire <robin.lemaire@adevinta.com>
2024-06-12 15:16:18 +0200

    Migrate from spark-ios

---
+++ Sources/SparkBadge/Properties/Private/BadgeColors.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeColors.swift
+//  Spark
+//
+//  Created by alex.vecherov on 04.05.23.
+//  Copyright © 2023 Adevinta. All rights reserved.
+//
+
+import SparkTheming
+
+// sourcery: AutoMockable
+struct BadgeColors {
+
+    // MARK: - Properties
+
+    let backgroundColor: any ColorToken
+    let borderColor: any ColorToken
+    let foregroundColor: any ColorToken
+}
+
